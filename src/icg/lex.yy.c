7f864769336a1534e89f5aa4b6d23f7db59f80ff
JoyTheSloth <112154016+JoyTheSloth@users.noreply.github.com>
2025-04-17 17:11:07 +0530

    Add files via upload

---
+++ src/icg/lex.yy.c
@@ -0,0 +1,3 @@
+/* Dummy lex.yy.c file for testing */
+#include <stdio.h>
+/* This is just a placeholder. You need the actual generated file to compile properly. */
---
+++ src/icg/minic.tab.h
@@ -0,0 +1,5 @@
+/* Dummy minic.tab.h file for testing */
+#ifndef MINIC_TAB_H
+#define MINIC_TAB_H
+/* This is just a placeholder. You need the actual generated file to compile properly. */
+#endif /* MINIC_TAB_H */
